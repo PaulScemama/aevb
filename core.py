5a69495be2a9eda00f0521b2f2fe3b6574fe842b
Paul Scemama <ptscemama@gmail.com>
2024-01-31 01:52:30 +0000

    Flesh out core functions; Test some in notebook

--- core.py
+++ core.py
@@ -1,11 +1,11 @@
-from typing import Union, Iterable, Mapping, Any
+from typing import Union, Iterable, Mapping, Any, Callable, NamedTuple
 from jax.typing import ArrayLike
 
 import jax
 import jax.numpy as jnp
 from jax.random import PRNGKey
-import jax.scipy.stats as stats
-
+import optax
+from optax import GradientTransformation, OptState
 
 Array = jnp.array
 from jax.tree_util import tree_leaves, tree_structure, tree_unflatten
@@ -31,9 +31,22 @@ References:
 """
 
 
+class AEVBState(NamedTuple):
+    rec_params: ArrayLikeTree
+    gen_params: ArrayLikeTree
+    rec_opt_state: OptState
+    gen_opt_state: OptState
+
+
+class AEVBInfo(NamedTuple):
+    loss: float
+    nll: float
+    kl: float
+
+
 # Useful PyTree Utility: modified from https://github.com/google-research/google-research/blob/master/bnn_hmc/utils/tree_utils.py
 # to allow for `n_samples`` to be taken.
-def _gaussian_like_tree(
+def normal_like_tree(
     rng_key: PRNGKey, tree: ArrayLikeTree, n_samples: int
 ) -> ArrayTree:
     treedef = tree_structure(tree)
@@ -47,20 +60,19 @@ def _gaussian_like_tree(
     return noise
 
 
-def _gaussian_loglikelihood_fn(apply_fn, params, batch):
-    """Pass use the parameters with the inputs from batch along
-    with the apply_fn to predict an output. Then compute the normal loglikelihood
-    of the observed data from batch under the model with the predicted output"""
-    x, y = batch
-    predicted_loc = apply_fn(params, x)
-    return stats.norm.logpdf(y, loc=predicted_loc)
+def normal_loglikelihood_fn(apply_fn, params, z, x):
+    """Use the `params` along with the `apply_fn` to predict `x` from `z`. Then
+    compute the observed data `x` under a normal distribution parameterized by the
+    predicted `x` as its mean."""
+    pred_x = apply_fn(params, z)
+    return -((x - pred_x) ** 2)
 
 
-def _reparameterized_sample_loc_scale(
+def reparameterized_sample_loc_scale(
     rng_key: PRNGKey, loc: ArrayLikeTree, scale: ArrayLikeTree, n_samples: int
 ) -> ArrayTree:
     """Compute a sample from a loc-scale family distribution using the reparameterization trick."""
-    noise = _gaussian_like_tree(rng_key, loc, n_samples)
+    noise = normal_like_tree(rng_key, loc, n_samples)
     samples = jax.tree_map(
         lambda loc, scale, noise: loc + scale * noise,
         loc,
@@ -70,43 +82,93 @@ def _reparameterized_sample_loc_scale(
     return samples
 
 
-def _unit_gaussian_kl(mu, sigma):
+def unit_normal_kl(mu, sigma):
     """As per Appendix B of [1]"""
 
     def kl(mu, sigma):
         mu_squared = jnp.square(mu)
         sigma_squared = jnp.square(sigma)
-        return (1 + jnp.log(sigma_squared) - mu_squared - sigma_squared) / 2
+        return -(1 + jnp.log(sigma_squared) - mu_squared - sigma_squared) / 2
 
     kl_tree = jax.tree_map(kl, mu, sigma)
     kl_val = sum([param_kl.sum() for param_kl in tree_leaves(kl_tree)])
     return kl_val
 
 
-def _analytical_kl_svgb_estimator(
-    rec_params, gen_params, rec_apply_fn, gen_apply_fn, batch
-):
-    x, y = batch
+def tractable_kl_step(
+    rng_key: PRNGKey,
+    aevb_state: AEVBState,
+    rec_apply_fn: Callable,
+    gen_apply_fn: Callable,
+    x: ArrayLike,
+    optimizer: GradientTransformation,
+    n_samples: int,
+) -> tuple[AEVBState, AEVBInfo]:
+    """_summary_
+
+    Args:
+        rng_key (PRNGKey): _description_
+        aevb_state (AEVBState): _description_
+        rec_apply_fn (Callable): _description_
+        gen_apply_fn (Callable): _description_
+        x (ArrayLike): _description_
+        optimizer (GradientTransformation): _description_
+        n_samples (int): _description_
+
+    Returns:
+        tuple[AEVBState, AEVBInfo]: _description_
+    """
+    rec_params, gen_params = aevb_state.rec_params, aevb_state.gen_params
     params = (rec_params, gen_params)
 
     def loss_fn(params) -> float:
         rec_params, gen_params = params
 
-        # Predicted parameters for latent variables from recognition model
-        rec_mu, rec_sigma = rec_apply_fn(rec_params, x)
-        # Sample a z using its predicted loc and scale
-        z = _reparameterized_sample_loc_scale(...)
-        # Predicted parameter mu for predicting the data from the latent variable z
-        gen_mu = gen_apply_fn(gen_params, z)
+        # Compute the predicted parameters for z using the recognition model
+        # and input x.
+        pred_z_mu, pred_z_sigma = rec_apply_fn(rec_params, x)
+        # Sample a z using its predicted parameters.
+        z = reparameterized_sample_loc_scale(
+            rng_key, pred_z_mu, pred_z_sigma, n_samples
+        )
 
         # Compute the KL penalty between the prior over z (unit gaussian) and current
-        # recognition model posterior
-        # NOTE: might have to negative this
-        kl_penalty = _unit_gaussian_kl(rec_mu, rec_sigma)
-        # Compute reconstruction loss term
-        nll = _gaussian_loglikelihood_fn(gen_apply_fn, gen_params, batch)
+        # recognition model posterior.
+        kl = unit_normal_kl(pred_z_mu, pred_z_sigma)
+        # Compute negative log likelihood of the observed data under the generative model.
+        nll = -normal_loglikelihood_fn(gen_apply_fn, gen_params, z, x)
 
-    return jax.grad(loss_fn)(params)
+        loss = (nll + kl).mean()
+        return loss, (nll, kl)
+
+    loss_grad_fn = jax.value_and_grad(loss_fn)(params)
+
+    (loss_val, (nll, kl)), loss_grad = loss_grad_fn(params)
+
+    # Update recognition model parameters
+    rec_updates, new_rec_opt_state = optimizer.update(
+        loss_grad,
+        aevb_state.rec_opt_state,
+        rec_params,
+    )
+    new_rec_params = optax.apply_updates(rec_params, rec_updates)
+
+    # Update generative model parameters
+    gen_updates, new_gen_opt_state = optimizer.update(
+        loss_grad,
+        aevb_state.gen_opt_state,
+        gen_params,
+    )
+    new_gen_params = optax.apply_updates(gen_params, gen_updates)
+
+    new_aevb_state = AEVBState(
+        new_rec_params,
+        new_gen_params,
+        new_rec_opt_state,
+        new_gen_opt_state,
+    )
+
+    return new_aevb_state, AEVBInfo(loss_val, nll, kl)
 
 
 ## NOTE: recognition model must prediction a mu and sigma from a data point x.
